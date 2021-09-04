03b7398fef070076cfd15d17ee8c138dd850cae9
sworc <lightbox@nifty.com>
2021-09-04 21:20:54 +0900

    Update Main.java

--- pg1-map-getenv/Main.java
+++ pg1-map-getenv/Main.java
@@ -12,6 +12,19 @@ public class Main {
             String out = String.format( "%s : %s", key, env.get( key ) );
             System.out.println( out);
         }
+        
+        HashMap<String, String> mymap = new HashMap<String, String>();
+        mymap.put("言語1", "Java");
+        mymap.put("言語2", "JavaScript");
+        mymap.put("言語3", "C#");
+        mymap.put("言語4", "PHP");
+
+        iterator = mymap.keySet().iterator();
+        while ( iterator.hasNext() ) {
+            String key = iterator.next();
+            String out = String.format( "%s : %s", key, mymap.get( key ) );
+            System.out.println( out );
+        }        
 
     }
 
