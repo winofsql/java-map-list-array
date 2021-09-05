8abf31478697d3c90d153cf3c690796825ec18f7
sworc <lightbox@nifty.com>
2021-09-05 15:33:36 +0900

    Update Main.java

--- pg2-getenv-to-array/Main.java
+++ pg2-getenv-to-array/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         // 通常 for
         for( int i = 0; i < keys.length; i++ ) {
-            String value = keys[i];
+            String key_value = keys[i];
             String out = String.format( "%s : %s", key, env.get( key ) );
             System.out.println( out );
         }
